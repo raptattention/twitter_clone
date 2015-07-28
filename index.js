641052b3a232f8ee0d72c71c7b2bf6ddcdf282a1
raptattention <klaw1794@gmail.com>
2015-07-28 15:00:24 +0800

    feat(getting-started) Add basic server configurations

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
+// Hpai is class
 var Hapi = require('hapi');
 
+// Instantiate
 var server = new Hapi.Server();
 
+// Configure server connections / host
 server.connection({
-	host: 'localhost',
-	port: 8000
-});
-
-server.route({
-	method: 'GET',
-	path: '/hello',
-	handler: function(request, reply){
-		reply('hello world!');
+	host: '0.0.0.0',
+	port: 3000,
+	routes: {
+		cors: {
+			headers: ["Access-Control-Allow-Credentials"],
+			credentials: true
+		}
 	}
 });
 
-server.start();
+var plugins = [
+	// Require MongoDB
+	{
+		register: require('hapi-mongodb'),
+		options:{
+			url: "mongodb://127.0.0.1:27017/twitter_clone",
+			settings: {
+				db: {
+					native_parser: false
+				}
+			}
+		}
+	}
+];
+
+// Print "hello world!" on /hello
+// server.route({
+// 	method: 'GET',
+// 	path: '/hello',
+// 	handler: function(request, reply){
+// 		reply('hello world!');
+// 	}
+// });
+
+// Start server
+server.register(plugins, function(err){
+	// check error
+	if (err){
+		throw err;
+	}
+
+	// start server
+	server.start(function(){
+		console.log("info", "Server running at: "+server.info.uri);
+	})
+});
